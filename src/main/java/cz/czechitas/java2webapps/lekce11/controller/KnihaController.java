483519ded25e6319003f28e47b96e2fd16cd1341
Dasa <dasa.podkulova@gmail.com>
2024-06-18 20:19:42 +0200

    Mazání, obnovení, přidání knih

--- src/main/java/cz/czechitas/java2webapps/lekce11/controller/KnihaController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce11/controller/KnihaController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Filip Jirsák
@@ -37,8 +38,29 @@ public class KnihaController {
         return service.pridat(kniha);
     }
 
+    @GetMapping("/{id}")
+    public Optional<Kniha> detail(@PathVariable long id) {
+        return service.detail(id);
+    }
+
     @PostMapping("/batch")
     public List<Kniha> pridatDavkove(@RequestBody List<Kniha> kniha) {
         return service.pridatDavkove(kniha);
+    }
+
+    @PutMapping("/{id}")
+    public Kniha upravit(@PathVariable long id, @RequestBody Kniha kniha) {
+        kniha.setId(id);
+        return service.upravit(kniha);
+    }
+
+    @DeleteMapping("/{id}")
+    public Kniha smazat(@PathVariable long id) {
+        return service.smazat(id);
+    }
+
+    @PostMapping("/{id}/obnovit")
+    public Kniha obnovit(@PathVariable long id) {
+        return service.obnovit(id);
     }
 }
